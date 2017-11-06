8b20e65322889e22d50be67f8969ce35eee81995
Seth Tippetts <sethtippetts@gmail.com>
2017-11-05 22:36:25 -0700

    feature: stripe worker to keep stripe data up to date with ours

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ require('dotenv').load()
 // Services
 require('@nerdsauce/event.create')
 require('@nerdsauce/event.query')
+require('@nerdsauce/event.stripe')
 require('@nerdsauce/graphql.gateway')
