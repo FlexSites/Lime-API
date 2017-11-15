81767fdf79900da6bb6bd0e80598c9fe49a9849a
Seth Tippetts <sethtippetts@gmail.com>
2017-11-14 21:49:49 -0700

    chore: upgrade to version 0.3.1 of Conduit

--- services/service.order.query/index.js
+++ services/service.order.query/index.js
@@ -1,6 +1,44 @@
-const AMQP = require('@nerdsauce/amqp')
-const Monk = require('monk')
+const Conduit = require('@nerdsauce/conduit')
+const { promisify } = require('util')
+const Stripe = require('stripe')
+const { debug } = require('@nerdsauce/conduit/middleware/debug')
 
-const { worker } = require('./worker')
+const stripe = new Stripe(process.env.STRIPE_TOKEN)
+const conduit = new Conduit(process.env.AMQP_URL, { name: 'service.order.query' })
 
-worker(new Monk(process.env.MONGODB_URL), new AMQP(process.env.AMQP_URL, { name: 'order.create.service' }))
+const listOrders = promisify(stripe.orders.list.bind(stripe.orders))
+
+conduit
+  .middleware(debug)
+  .middleware(context => next => (args, method) => {
+    return next(args, method)
+      .catch(ex => console.error(ex))
+  })
+  .reaction('order.query.v1', async (msg, message) => {
+    console.log('order.query.v1')
+    const { data: orders } = await listOrders(msg)
+    return orders
+      .map(({ amount, id, items, created, email, status }) => {
+        const tickets = [].concat(
+          ...order
+            .items
+            .filter(({ type }) => type === 'sku')
+            .map(item => {
+              return new Array(item.quantity).fill({
+                id: order.id,
+                order_id: order.id,
+                type: 'GA'
+              })
+            })
+        )
+        return {
+          id: order.id,
+          created: order.created,
+          amount,
+          status,
+          user: { email },
+          amount,
+          tickets
+        }
+      })
+  })
