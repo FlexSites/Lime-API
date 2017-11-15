e5653f3c70b2a06246588e2100319fc76df5b334
Seth Tippetts <sethtippetts@gmail.com>
2017-11-14 18:43:14 -0700

    fix: better authentication handling

--- services/gateway.graphql/auth.js
+++ services/gateway.graphql/auth.js
@@ -1,14 +1,35 @@
-var jwt = require('express-jwt')
-var jwks = require('jwks-rsa')
+const AuthenticationClient = require('auth0').AuthenticationClient
+const ManagementClient = require('auth0').ManagementClient
+const get = require('lodash.get')
 
-module.exports = jwt({
-  secret: jwks.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: 'https://ticketing.auth0.com/.well-known/jwks.json'
-  }),
-  audience: 'https://lime.events',
-  issuer: 'https://ticketing.auth0.com/',
-  algorithms: ['RS256']
+const manage = new ManagementClient({
+  clientId: process.env.AUTH0_MANAGEMENT_CLIENT,
+  domain: process.env.AUTH0_DOMAIN,
+  clientSecret: process.env.AUTH0_MANAGEMENT_SECRET
 })
+
+const auth = new AuthenticationClient({
+  domain: process.env.AUTH0_DOMAIN,
+  clientId: process.env.AUTH0_AUTHENTICATION_CLIENT
+})
+
+const UNAUTHENTICATED_USER = {
+  groups: [],
+  roles: ['everyone'],
+  permissions: ['read:event', 'read:venue']
+}
+
+module.exports = async (token) => {
+  try {
+    if (!token) throw new Error('No token provided.')
+    token = token.replace(/^Bearer\s/i, '')
+    const results = await auth.getProfile(token)
+    if (typeof results !== 'object' || !results.sub) {
+      throw new Error('Bad token.')
+    }
+    const viewers = await manage.getUser(results.sub)
+    return get(viewers, ['0', 'app_metadata', 'authorization'], UNAUTHENTICATED_USER)
+  } catch (ex) {
+    return UNAUTHENTICATED_USER
+  }
+}
--- services/gateway.graphql/graphql.js
+++ services/gateway.graphql/graphql.js
@@ -4,21 +4,7 @@ const uuid = require('uuid')
 const graphqlExpress = require('express-graphql')
 const Conduit = require('@nerdsauce/conduit')
 
-const AuthenticationClient = require('auth0').AuthenticationClient
-const ManagementClient = require('auth0').ManagementClient
-const get = require('lodash.get')
-// const axios = require('axios')
-
-const manage = new ManagementClient({
-  clientId: 'WQTjdPVvXuIa0gHmzcYu5Fe6lkiqD8FL',
-  domain: 'ticketing.auth0.com',
-  clientSecret: 'vslUvK5xgHnGUD6cqrFjRNdl5Cb2JWby68E43w4686m5R-bLf14z-PKLXDZDHnpm'
-})
-
-const auth = new AuthenticationClient({
-  domain: 'ticketing.auth0.com',
-  clientId: 'D3QDBTxJhSqEmiSKRUJOk6SXSYfUUdp5'
-})
+const auth = require('./auth')
 
 const schemaPromise = require('./schema')()
 
@@ -38,23 +24,13 @@ const conduit = new Conduit(process.env.AMQP_URL, { name: 'graphql.service' })
 const app = express()
 
 app.use(cors())
-// app.use(auth())
 app.use(async (req, res, next) => {
-  if (req.method !== 'POST') {
-    return next()
-  }
-  try {
-    const token = req.get('authorization')
-    if (!token) return next()
+  if (req.method === 'POST') {
     console.time('auth')
-    const { sub } = await auth.getProfile(token.replace(/^Bearer\s/i, ''))
-    const viewers = await manage.getUser(sub)
-    res.locals.viewer = get(viewers, ['0', 'app_metadata', 'authorization'], {})
+    res.locals.viewer = await auth(req.get('authorization'))
     console.timeEnd('auth')
-    next()
-  } catch (ex) {
-    next(ex)
   }
+  return next()
 })
 app.use(graphqlExpress(async (req, res) => {
   const schema = await schemaPromise
@@ -73,5 +49,13 @@ app.use(graphqlExpress(async (req, res) => {
     }
   }
 }))
+app.use((err, req, res, next) => {
+  res.send({
+    errors: [{
+      message: err.message,
+      stack: err.stack
+    }]
+  })
+})
 
 module.exports = app
