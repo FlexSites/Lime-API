2cf3dfd013419b99925f7cff468ce6b4d08296b2
Seth Tippetts <sethtippetts@gmail.com>
2017-11-14 18:41:46 -0700

    chore: remove unused `order.linkStripe`

--- services/service.order.linkStripe/index.js
+++
@@ -1,6 +0,0 @@
-const AMQP = require('@nerdsauce/amqp')
-const Monk = require('monk')
-
-const { worker } = require('./worker')
-
-worker(new Monk(process.env.MONGODB_URL), new AMQP(process.env.AMQP_URL, { name: 'order.create.service' }))
